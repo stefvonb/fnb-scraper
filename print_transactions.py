c6be5a743dbe0a6dc16d206b01a2056e8f1bfab4
Stefan von Buddenbrock <stefan@von-buddenbrock.com>
2022-11-19 20:33:10 +0100

    Code cleanup I think

--- print_transactions.py
+++ print_transactions.py
@@ -16,16 +16,21 @@ parser.add_argument('-u', '--username', help="Your FNB username.")
 parser.add_argument('-p', '--password', help="Your FNB password.")
 args = parser.parse_args()
 
+
 def main():
     """
-    Runs the program and prints out a list of transactions that have been scraped.
+    Runs the program and prints out a list of transactions that have been
+    scraped.
     """
     log.info("Starting FNB scraper.")
-    username = os.environ["FNB_USERNAME"] if args.username is None else args.username
-    password = os.environ["FNB_PASSWORD"] if args.password is None else args.password
+    username = os.environ["FNB_USERNAME"] if args.username is None \
+        else args.username
+    password = os.environ["FNB_PASSWORD"] if args.password is None \
+        else args.password
     fnb_scraper = FNBScraper(username, password)
     fnb_scraper.scrape()
     print(fnb_scraper.get_transactions())
+
 
 if __name__ == "__main__":
     main()
